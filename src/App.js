cdacbd64c3d149f9f7408aefde99a142a10d7837
Weslley Neri <weslley39@gmail.com>
2019-04-16 22:32:51 -0300

    useEffect with custom hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useTitleInput } from "./hooks/useTitleInput";
 
 const App = () => {
-  const [name, setName] = useState('');
+  const [name, setName] = useTitleInput("");
 
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
-      <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
-      { name }
+      <input type="text" onChange={e => setName(e.target.value)} value={name} />
+      {name}
     </div>
   );
 };
