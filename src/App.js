f4ad6ef95f552d6cbe6312ae172c51458ee5d37d
Weslley Neri <weslley39@gmail.com>
2019-04-16 22:48:26 -0300

    useRef

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import { useTitleInput } from "./hooks/useTitleInput";
 
 const App = () => {
   const [name, setName] = useTitleInput("");
+  const ref = useRef();
 
   return (
-    <div className="main-wrapper">
-      <h1>Level Up Dishes</h1>
+    <div className="main-wrapper" ref={ref}>
+      <h1 onClick={() => ref.current.classList.add("new-fake-class")}>
+        Level Up Dishes
+      </h1>
       <input type="text" onChange={e => setName(e.target.value)} value={name} />
       {name}
     </div>
