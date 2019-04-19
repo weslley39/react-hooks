716a81d2bab14432cbefbc0e3c5f2421cbd45043
Weslley Neri <weslley39@gmail.com>
2019-04-19 00:56:28 -0300

    useEffect as componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import { useTitleInput } from "./hooks/useTitleInput";
 import Counter from "./Counter";
 import ReverseWord from "./ReverseWord";
 
 const App = () => {
   const [name, setName] = useTitleInput("");
+  const [dishes, setDishes] = useTitleInput([]);
   const ref = useRef();
+
+  const fetchDishes = async () => {
+    const res = await fetch(
+      "https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes"
+    );
+    const data = await res.json();
+    setDishes(data);
+  };
+
+  useEffect(() => {
+    fetchDishes();
+  }, []); //[] will simulate componentDidMount
 
   return (
     <div className="main-wrapper" ref={ref}>
@@ -16,6 +29,18 @@ const App = () => {
       <div>{name}</div>
       <ReverseWord value={name} />
       <Counter />
+
+      {dishes.map(dish => (
+        <article className="dish-card dish-card--withImage">
+          <h3>{dish.name}</h3>
+          <p>{dish.desc}</p>
+          <div className="ingredients">
+            {dish.ingredients.map(ingredient => (
+              <span>{ingredient}</span>
+            ))}
+          </div>
+        </article>
+      ))}
     </div>
   );
 };
