966ccdf10730eadb5604c82b8a65ae8b02a716fa
Weslley Neri <weslley39@gmail.com>
2019-04-22 13:36:18 -0300

    cleanup function hook

--- src/DishForm.js
+++ src/DishForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useBodyScrollLock } from "./hooks/useBodyScrollLock";
 
 const DishForm = () => {
-  // useBodyScrollLock();
+  useBodyScrollLock();
   return (
     <div className="dish-card">
       <form>
--- src/hooks/useBodyScrollLock.js
+++ src/hooks/useBodyScrollLock.js
@@ -2,8 +2,13 @@ import { useLayoutEffect } from "react";
 
 function useBodyScrollLock() {
   useLayoutEffect(() => {
+    const originalOverflow = window.getComputedStyle(document.body).overflow;
     document.body.style.overflow = "hidden";
-  });
+
+    return () => {
+      document.body.style.overflow = originalOverflow;
+    };
+  }, []);
 }
 
 export { useBodyScrollLock };
